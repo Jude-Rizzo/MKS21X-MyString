2dd8d3a65cf1d4d59351bbfb4bccf9c5308252c2
Jude-Rizzo <jprizzo2020@gmail.com>
2018-11-21 11:36:57 -0500

    horray

--- MyString.java
+++ MyString.java
@@ -2,15 +2,32 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
   public MyString(CharSequence s){
     data = new char[s.length()];
-    for(int i = 0; i < s.length(); s++){
+    for(int i = 0; i < s.length(); i++){
       data[i] = s.charAt(i);
     }
   }
 
-  public char charAt(){
+  public char charAt(int i){
+      return data[i];
+  }
+
+  public int length(){
+    return data.length;
+  }
+
+  public CharSequence subSequence(int s, int l){
+    for(int i = s; i < l; i++){
+      
+    }
+  }
+
+  public int compareTo(CharSequence T){
 
   }
 
-  
+  public String toString(){
+
   }
-}
+
+
+  }
