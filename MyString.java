ace6f369564c4437ca5f90aee0f85d0cc44ba829
Jude Rizzo <jrizzo00@stuy.edu>
2018-11-25 14:46:06 -0700

    compareto done

--- MyString.java
+++ MyString.java
@@ -1,6 +1,6 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
-  
+
   public MyString(CharSequence s){
     data = new char[s.length()];
     for(int i = 0; i < s.length(); i++){
@@ -25,6 +25,7 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
     //loop through and copy over all the chars within the bounds
     if(l < s || s > data.length || s < 0 || l < 0 )
       throw new IndexOutOfBoundsException();
+
     //dont forget to check for errors
     String ans = "";
     for(int i = s; i < l; i++){
@@ -34,6 +35,35 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
   }
 
   public int compareTo(CharSequence T){
+    //get null pointer exception out of the way
+    if(T == null) throw new NullPointerException();
+    //If the MyStrings are different length return the difference of the first chars
+    if(T.length == 0) return data.length();
+      //s is the minumum length
+      int s;
+      if(T.length() < this.length()){
+        s = T.length();
+      } else {
+        s = this.length();
+      }
+      //now compare the first difference in chars
+      //if there isnt one return the difference in length
+      int counter = 0;
+      for(int i = 0; i < s; i ++){
+        if(data[i] != T.charAt(i))
+          return data[i] - T.charAt(i);
+        counter++
+        if(counter == s)
+          return this.length() - T.length();
+      }
+
+
+  }
+
+
+
+
+
 
   }
 
