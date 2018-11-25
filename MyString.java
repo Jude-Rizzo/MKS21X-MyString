6a4f416f0a5b6e0c692602311a9c268481183ea9
Jude Rizzo <jrizzo00@stuy.edu>
2018-11-25 14:35:31 -0700

    working on compareTo

--- MyString.java
+++ MyString.java
@@ -1,5 +1,6 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
+  
   public MyString(CharSequence s){
     data = new char[s.length()];
     for(int i = 0; i < s.length(); i++){
@@ -8,6 +9,7 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
   }
 
   public char charAt(int i){
+    //pretty straightfoward just go to that character
     if(i < 0 || i >= data.length){
       throw new IndexOutOfBoundsException();
     }
@@ -15,10 +17,15 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
   }
 
   public int length(){
+    //just array length
     return data.length;
   }
 
   public CharSequence subSequence(int s, int l){
+    //loop through and copy over all the chars within the bounds
+    if(l < s || s > data.length || s < 0 || l < 0 )
+      throw new IndexOutOfBoundsException();
+    //dont forget to check for errors
     String ans = "";
     for(int i = s; i < l; i++){
     ans += data[i];
